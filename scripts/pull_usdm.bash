be1e5e3fee22a2675b0b0c979f0bb11877cf2713
Geoff Low <glow@mdsol.com>
2023-03-10 15:52:22 +0000

    Pull CT

--- scripts/pull_usdm.bash
+++ scripts/pull_usdm.bash
@@ -14,3 +14,10 @@ curl -LJO $REMOTE --output-dir $INPUTDIR
 
 # Rename the file to match the branch name
 mv $INPUTDIR/USDM_UML.eapx $INPUTDIR/${BRANCH}_USDM_UML.eapx
+
+REMOTE_CT="https://github.com/cdisc-org/DDF-RA/raw/${BRANCH}/Deliverables/CT/USDM_CT.xlsx"
+
+echo "Pulling USDM CT from ${REMOTE_CT}"
+
+curl -LJO $REMOTE_CT --output-dir $INPUTDIR 
+
