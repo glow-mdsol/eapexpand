4ba17ef391fcc6f984739e5e7d147c1c81c22e8f
Geoff Low <glow@mdsol.com>
2023-03-20 11:17:52 +0000

    Add tabs for controlled terminology

--- src/eapexpand/usdm_unpkt.py
+++ src/eapexpand/usdm_unpkt.py
@@ -4,7 +4,7 @@ import re
 from openpyxl import load_workbook, Workbook
 from openpyxl.cell.cell import Cell
 from dataclasses import dataclass, field
-from typing import List, Optional
+from typing import Dict, List, Optional
 import os
 from .unpkt import load_expanded_dir
 
@@ -69,7 +69,7 @@ class Entity:
             if attr.logical_data_model_name == attribute_name:
                 return attr
         return None
-    
+
     @classmethod
     def from_row(cls, row):
         if row[6]:
@@ -88,7 +88,7 @@ class Entity:
         if has_value_list:
             if has_value_list.startswith("Y"):
                 entity.has_value_list = True
-                source = re.compile(r'Y \((.*)\)$')
+                source = re.compile(r"Y \((.*)\)$")
                 m = source.match(row[8])
                 if m:
                     entity.value_list_description = m.group(1)
@@ -98,10 +98,31 @@ class Entity:
                         entity.external_value_list = False
                         entity.codelist_c_code = entity.value_list_description
                 else:
-                    raise ValueError(f"Unable to parse value list description: {row[8]}")
+                    raise ValueError(
+                        f"Unable to parse value list description: {row[8]}"
+                    )
             elif has_value_list.startswith("N"):
                 entity.has_value_list = False
         return entity
+
+class CodeList:
+    def __init__(self, entity_name: str, attribute: str, codelist_c_code: str):
+        self.entity_name = entity_name
+        self.attribute = attribute
+        self.codelist_c_code = codelist_c_code
+        self.items = []
+
+    def add_item(self, item: PermissibleValue):
+        self.items.append(item)
+
+    @classmethod
+    def from_pvalue(cls, pvalue: PermissibleValue):
+        return cls(
+            entity_name=pvalue.entity,
+            attribute=pvalue.attribute,
+            codelist_c_code=pvalue.codelist_c_code,
+        )
+
 
 
 def load_usdm_ct(filename: str):
@@ -123,21 +144,29 @@ def load_usdm_ct(filename: str):
         else:
             raise ValueError(f"Unknown entity type: {row[2]}")
     _value_sets = wbk["DDF valid value sets"]
+    codelists = {}
     for row in _value_sets.iter_rows(min_row=7, values_only=True):
         # get the entity
         codeset = PermissibleValue.from_row(row)
+        if codeset.codelist_c_code not in codelists:
+            codelists[codeset.codelist_c_code] = CodeList.from_pvalue(codeset)
+        codelists[codeset.codelist_c_code].add_item(codeset)
+        # get the entity
         _entity = entities[row[1]]
+        # get the attribute
         _attribute = row[2]
         if _attribute == "encounterContactMode":
             print("WARNING Remap encounterContactMode to encounterContactModes")
             _attribute = "encounterContactModes"
         _attr = _entity.get_attribute(_attribute)
         if _attr is None:
-            raise ValueError(f"Attribute {row[2]} not found in entity {_entity.entity_name} ")
+            raise ValueError(
+                f"Attribute {row[2]} not found in entity {_entity.entity_name} "
+            )
         if _attr.codelist_c_code is None:
             _attr.codelist_c_code = codeset.codelist_c_code
         _attr.codelist_items.append(codeset)
-    return entities
+    return entities, codelists
 
 
 def generate(
@@ -146,17 +175,30 @@ def generate(
     attributes: dict,
     connectors: dict,
     ct_content: dict,
+    codelists: dict,
     output_dir: Optional[str] = "output",
 ):
     """
     Generates the Excel Representation of the model
     :param name: The name of the model - guides what the output file is called
     """
+    # remap the code attribute model to the code list model
     doc = Workbook()
     sheet = doc.active
-    sheet.title = "Objects"
+    sheet.title = "Core Model"
     for idx, column in enumerate(
-        ("Class", "Attribute", "Type", "Cardinality", "Class Note", "NCI C-code", "Preferred term", "Definition", "Codelist", "External Codelist")
+        (
+            "Class",
+            "Attribute",
+            "Type",
+            "Cardinality",
+            "Class Note",
+            "NCI C-code",
+            "Preferred term",
+            "Definition",
+            "Codelist",
+            "External Codelist",
+        )
     ):
         sheet.cell(row=1, column=idx + 1).value = column
     row_num = 2
@@ -164,9 +206,9 @@ def generate(
         _output = {}
         if obj.object_type == "Class":
             _object_id = obj.object_id
-            _ref = ct_content.get(obj.name) # type: Entity
+            _ref = ct_content.get(obj.name)  # type: Entity
             if _ref is None:
-                print("WARNING: Reference not found for " + obj.name)             
+                print("WARNING: Reference not found for " + obj.name)
             _attributes = sorted(
                 [
                     attributes[attr_id]
@@ -207,9 +249,13 @@ def generate(
                             attrib["pref_term"] = _attr_ref.preferred_term
                             if _attr_ref.has_value_list:
                                 if _attr_ref.external_value_list:
-                                    attrib["external_value_list"] = _attr_ref.value_list_description
+                                    attrib[
+                                        "external_value_list"
+                                    ] = _attr_ref.value_list_description
                                 else:
-                                    attrib["codelist"] = _attr_ref.value_list_description or ""
+                                    attrib["codelist"] = (
+                                        _attr_ref.value_list_description or ""
+                                    )
                 _output[_attribute.name] = attrib
             connections = [
                 connectors[cid]
@@ -229,6 +275,12 @@ def generate(
                     )
                 _output[connection.name] = attrib
             for offset, attrib in enumerate(_output.values()):
+                _codelist = attrib.get("codelist")
+                if _codelist:
+                    _cl = ct_content.get(_codelist)
+                    _codelist_value = '=HYPERLINK("#{}!A2","{}")'.format(_codelist, _codelist)
+                else:
+                    _codelist_value = ""
                 sheet.cell(row=row_num, column=1).value = obj.name
                 sheet.cell(row=row_num, column=2).value = attrib["attribute_name"]
                 sheet.cell(row=row_num, column=3).value = attrib["attribute_type"]
@@ -238,9 +290,25 @@ def generate(
                 sheet.cell(row=row_num, column=6).value = attrib.get("c_code", "")
                 sheet.cell(row=row_num, column=7).value = attrib.get("pref_term", "")
                 sheet.cell(row=row_num, column=8).value = attrib.get("definition", "")
-                sheet.cell(row=row_num, column=9).value = attrib.get("codelist", "")
-                sheet.cell(row=row_num, column=10).value = attrib.get("external_value_list", "")
+                sheet.cell(row=row_num, column=9).value = _codelist_value
+                sheet.cell(row=row_num, column=10).value = attrib.get(
+                    "external_value_list", ""
+                )
                 row_num += 1
+    for c_code, _codelist in codelists.items():
+        if c_code == 'CNEW':
+            continue
+        sheet = doc.create_sheet(c_code)
+        sheet.cell(row=1, column=1).value = "Code"
+        sheet.cell(row=1, column=2).value = "Preferred Term"
+        sheet.cell(row=1, column=3).value = "Synonyms"
+        sheet.cell(row=1, column=4).value = "Definition"
+        for idx, item in enumerate(_codelist.items):
+            # type item: PermissibleValue
+            sheet.cell(row=idx + 2, column=1).value = item.concept_c_code
+            sheet.cell(row=idx + 2, column=2).value = item.preferred_term
+            sheet.cell(row=idx + 2, column=3).value = ";".join(item.synonyms) if item.synonyms else ""
+            sheet.cell(row=idx + 2, column=4).value = item.definition
     # create the output directory if it doesn't exist
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
@@ -251,6 +319,10 @@ def generate(
 
 def main_usdm(source_dir: str, controlled_term: str, output_dir: str):
     objects, attributes, connectors = load_expanded_dir(source_dir=source_dir)
-    ct_content = load_usdm_ct(controlled_term)
-    name = os.path.basename(os.path.dirname(source_dir)) if source_dir.endswith("/") else os.path.basename(source_dir)
-    generate(name, objects, attributes, connectors, ct_content, output_dir)
+    ct_content, codelists = load_usdm_ct(controlled_term)
+    name = (
+        os.path.basename(os.path.dirname(source_dir))
+        if source_dir.endswith("/")
+        else os.path.basename(source_dir)
+    )
+    generate(name, objects, attributes, connectors, ct_content, codelists, output_dir)
