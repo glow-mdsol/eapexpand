80d6d7d54987eff9151714307d174cb8055d6ba8
Geoff Low <glow@mdsol.com>
2023-06-20 09:31:12 +0100

    entry point for schema generation

--- src/eapexpand/cli.py
+++ src/eapexpand/cli.py
@@ -8,7 +8,13 @@ def main():
     parser.add_argument("--usdm-ct", type=str, help="Path to Controlled Terms file")
     parser.add_argument("--output", type=str, help="Output directory", default="output")
     parser.add_argument("source", type=str, help="Source directory")
+    parser.add_argument("--prisma", help="Generate Prisma Schema", action="store_true", default=False)
+    parser.add_argument("--linkml", help="Generate LinkML Schema", action="store_true", default=False)
+    parser.add_argument("--shapes", help="Generate SHACL Schema", action="store_true", default=False)
     opts = parser.parse_args()
+    gen = dict(prisma=opts.prisma, 
+               linkml=opts.linkml, 
+               shapes=opts.shapes)
     source = opts.source
     output_dir = opts.output
     if opts.usdm:
@@ -16,8 +22,8 @@ def main():
             print("USDM Controlled Terms file is required")
             sys.exit(1)
         from .usdm_unpkt import main_usdm
-        main_usdm(opts.source, opts.usdm_ct, output_dir)
+        main_usdm(opts.source, opts.usdm_ct, output_dir, gen)
     else:
         from .unpkt import main
-        main(source, output_dir)
+        main(source, output_dir, gen)
 
