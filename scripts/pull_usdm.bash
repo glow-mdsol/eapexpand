e92b0f9137d0faaab215ddff0431cc896f8ff61d
Geoff Low <glow@mdsol.com>
2023-10-18 13:54:47 +0100

    point to new EA file

--- scripts/pull_usdm.bash
+++ scripts/pull_usdm.bash
@@ -7,14 +7,14 @@ if [ ! -d $INPUTDIR ]; then
     mkdir $INPUTDIR
 fi
 
-REMOTE="https://github.com/cdisc-org/DDF-RA/raw/${BRANCH}/Deliverables/UML/USDM_UML.eapx"
+REMOTE="https://github.com/cdisc-org/DDF-RA/raw/${BRANCH}/Deliverables/UML/USDM_UML.qea"
 
 echo "Pulling USDM from ${REMOTE}"
 
 curl -LJO $REMOTE --output-dir $INPUTDIR 
 
 # Rename the file to match the branch name
-mv $INPUTDIR/USDM_UML.eapx $INPUTDIR/${BRANCH}_USDM_UML.eapx
+mv $INPUTDIR/USDM_UML.qea $INPUTDIR/${BRANCH}_USDM_UML.qea
 
 REMOTE_CT="https://github.com/cdisc-org/DDF-RA/raw/${BRANCH}/Deliverables/CT/USDM_CT.xlsx"
 
