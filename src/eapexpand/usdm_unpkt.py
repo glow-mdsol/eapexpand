a7cd15078f734332fb5fa6ed3e5f9a07c8ce70d2
Geoff Low <glow@mdsol.com>
2024-01-03 13:00:02 +0000

    hard code an id for entity

--- src/eapexpand/usdm_unpkt.py
+++ src/eapexpand/usdm_unpkt.py
@@ -30,26 +30,31 @@ def load_usdm_ct(filename: str):
         _name = _row["Entity Name"]
         _role = _row["Role"]
         _entity = Entity.from_dict(_row)
-        if _entity.role == "Entity":
+        if _entity.role in ("Entity"):
             if not _name in entities:
+                # add an id attribute
+                _id = Entity.from_dict({"Entity Name": f"{_name}", 
+                                        "Role": "Attribute", 
+                                        "Logical Data Model Name": "id", 
+                                        "Definition": "Unique identifier for the entity", 
+                                        "CT Item Preferred Name": "id", 
+                                        "Has Value List": "N", 
+                                        "NCI C-code": "",
+                                        "Inherited From": ""})
+                _entity.attributes.append(_id)
                 entities[_name] = _entity
-        elif _entity.role == "Relationship" or _entity.role.startswith("Relationship ("):
+
+        elif _entity.role in ("Relationship"):
             entities[_name].relationships.append(_entity)
-        elif _entity.role == "Complex Datatype Relationship":
+        elif _entity.role in ("Complex Datatype Relationship"):
             entities[_name].complex_datatype_relationships.append(
                 _entity
             )
-        elif _entity.role in ("Attribute") or _role.startswith("Attribute ("):
-            # if "inherited from" in _role:
-            #     pattern = re.compile(r"\(inherited from (.*)[\)]?$")
-            #     if pattern.search(_role):
-            #         _inherited_from = pattern.search(_role).group(0)
-            #         _entity.inherited_from = _inherited_from
-            #     else:
-            #         print(f"Can't match {_role} for {_name}")
+        elif _entity.role in ("Attribute"):
             entities[_name].attributes.append(_entity)
         else:
-            raise ValueError(f"Unknown entity type: {row[2]}")
+            raise ValueError(f"Unknown entity type: {_role}")
+    print("Loading USDM CT Value Sets")
     _value_sets = wbk["DDF valid value sets"]
     codelists = {}
     missing_codelists = {}
@@ -59,26 +64,15 @@ def load_usdm_ct(filename: str):
         if codeset.codelist_c_code not in codelists:
             codelists[codeset.codelist_c_code] = CodeList.from_pvalue(codeset)
         codelists[codeset.codelist_c_code].add_item(codeset)
-        # get the entity
+        # get the entity name
         _entity = entities[row[1]]
-        # get the attribute
+        # get the attribute name
         _attribute = row[2]
         _attr = _entity.get_attribute(_attribute)
         # workaround
         if _attr is None:
-            # _unqualified_name = _attribute.replace(_entity.entity_name, '')
-            # _unqualified_name = _unqualified_name[0].lower() + _unqualified_name[1:]
-            # _attr = _entity.get_attribute(_unqualified_name)
-            # print(f"Warning: had to munge name: {_attribute} for {_entity.entity_name}")
-            # if _attr is None:
             missing_codelists[_attribute] = _entity.entity_name
-            # print(f"Attribute {row[2]} not found in entity {_entity.entity_name} ")
             continue
-            # raise ValueError(
-            #     f"Attribute {row[2]} not found in entity {_entity.entity_name} "
-            # )
-        if _attr.codelist_c_code is None:
-            _attr.codelist_c_code = codeset.codelist_c_code
         _attr.codelist_items.append(codeset)
     if missing_codelists:
         print("Missing Attributes")
@@ -100,16 +94,14 @@ def main_usdm(
         )
         document = load_expanded_dir(source_dir_or_file)
     ct_content, codelists = load_usdm_ct(controlled_term)
-    # name = os.path.basename(os.path.dirname(source_dir)) \
-    #     if source_dir.endswith("/") else os.path.basename(source_dir)
-    defintions = {}
+    definitions = {}
     for concept in ct_content.values():
         if concept.definition:
-            defintions[concept.entity_name] = concept.definition
+            definitions[concept.entity_name] = concept.definition
         for attr in concept.attributes:
             if attr.definition:
-                defintions[attr.logical_data_model_name] = attr.definition
-    document.merge_definitions(defintions)
+                definitions[attr.logical_data_model_name] = attr.definition
+    document.merge_definitions(definitions)
     for aspect, genflag in gen.items():
         print("Checking generation of ", aspect, "as", genflag)
         if genflag:
